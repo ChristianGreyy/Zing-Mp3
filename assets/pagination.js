a27f465dba032ee1bb32e5ce4dd94911f5aa8b17
Christian Grey <truongthanhhung2k2@gmail.com>
2021-12-11 13:24:05 +0700

    Update

---
+++ assets/pagination.js
@@ -0,0 +1,38 @@
+const menuListItem = document.querySelectorAll('.menu__list-item');
+let page = document.querySelectorAll('.page__page-items');
+
+console.log(menuListItem);
+
+const resetPage = (page) => {
+    page.forEach(pageItem => {
+        pageItem.style.display = 'none';
+    })
+    document.querySelector('.page__page-3__overlay').style.display = 'none';
+    document.querySelector('.page__page-3__alpha').style.display = 'none';
+    document.querySelector('.page__page-3__alpha2').style.display = 'none';
+
+    document.querySelector('.page__page-6__overlay').style.display = 'none';
+    document.querySelector('.page__page-6__alpha').style.display = 'none';
+    document.querySelector('.page__page-6__alpha2').style.display = 'none';
+}
+
+menuListItem.forEach(menuItem => {
+    menuItem.onclick = (e) => {
+        resetPage(page);
+        let pageNumber = menuItem.classList[1][17];
+        let classPage = "page__page-";
+        classPage += pageNumber;
+        if(pageNumber === '3' || pageNumber === '4') {
+            document.querySelector('.page__page-3__overlay').style.display = 'block';
+            document.querySelector('.page__page-3__alpha').style.display = 'block';
+            document.querySelector('.page__page-3__alpha2').style.display = 'block';
+        } else if(pageNumber === '6') {
+            document.querySelector('.page__page-6__overlay').style.display = 'block';
+            document.querySelector('.page__page-6__alpha').style.display = 'block';
+            document.querySelector('.page__page-6__alpha2').style.display = 'block';
+        }
+        currentPage = document.querySelector('.'.concat(classPage));
+        currentPage.style.display = 'block';
+        // console.log(currentPage);
+    }
+})
